f467e4e87df5c0f80e8bbd9f0d88907f7ccb7c88
gaurav-afk <gauravcreator123@gmail.com>
2024-02-11 23:22:37 -0500

    first commit

---
+++ ApexPredators/ApexPredator.swift
@@ -0,0 +1,8 @@
+//
+//  ApexPredetor.swift
+//  ApexPredators
+//
+//  Created by Gaurav Rawat on 2024-02-11.
+//
+
+import Foundation
---
+++ ApexPredators/PredatorDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorDetail.swift
+//  ApexPredators
+//
+//  Created by Gaurav Rawat on 2024-02-11.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorDetail()
+}
